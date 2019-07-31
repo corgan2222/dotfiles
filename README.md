165d866dc1a246d35a88fb0d5a34d1f4a1bc056d
Stefan Knaak <stefan@knaak.org>
2019-07-31 11:22:25 +0200

    functions and readme

--- README.md
+++ README.md
@@ -1,3 +1,114 @@
-# personal dotfiles
 
- bash <(curl https://corgan2222.github.io/dotfiles/deploy_homeshick.sh)
+# Dotfiles - bash Profile, Alias and basic Functions
+
+> Scripts and Functions
+
+all the Linux Stuff i used over the last 10 years
+
+![](https://corgan2222.github.io/dotfiles/checkout.jpg)
+
+## Table of Contents
+
+- [Dotfiles - Alias and basic Functions](#dotfiles---alias-and-basic-functions)
+- [Table of Contents](#table-of-contents)- [Requirements](#requirements)
+- [Installation](#installation)
+- [alias functions from /.dot/.bash_functions.sh](#alias-functions-from-dotbash_functionssh)
+- [homeshick native](#homeshick-native)
+- [Usage example](#usage-example)
+- [Release History](#release-history)
+- [Meta](#meta)
+
+## Requirements
+
+- linux or wsl
+- git
+- a lot of other stuff, check the functions
+
+## Installation
+
+Linux:
+
+```sh
+bash <(curl https://corgan2222.github.io/dotfiles/deploy_homeshick.sh)
+gitSaveCredential
+```
+
+## alias functions from /.dot/.bash_functions.sh
+
+Update:
+
+```sh
+saveHome
+```
+
+Load:
+
+```sh
+loadHome
+```
+
+Check:
+
+```sh
+checkHome
+```
+
+add File or Folder:
+
+```sh
+addToHome [file, folder]
+```
+
+## homeshick native
+
+Update:
+
+```sh
+homeshick cd dotfiles
+gitadd "message"
+gitp
+cd -
+```
+
+Load:
+
+```sh
+ homeshick pull dotfiles
+  reload
+```
+
+Check:
+
+```sh
+homeshick check dotfiles
+```
+
+add File or Folder:
+
+```sh
+homeshick track dotfiles [file, folder]
+```
+
+## Usage example
+
+_For alias and functions check the [Wiki][wiki]._
+
+## Release History
+
+- 0.0.1
+  - first deploy
+
+## Software Used and thanks
+
+- [homeshick (git dotfiles synchronizer written in bash)](https://github.com/andsens/homeshick)
+- [voku/dotfiles](https://github.com/voku/dotfiles)
+- [cowboy/dotfiles](https://github.com/cowboy/dotfiles)
+- [alrra/dotfiles](https://github.com/alrra/dotfiles)
+- []()
+- []()
+
+## Meta
+
+Stefan Knaak – stefan@knaak.org
+
+
--- home/.dot/.bash_functions.sh
+++ home/.dot/.bash_functions.sh
@@ -1172,6 +1172,97 @@ function shorturl()
     -d '{"longUrl": '\"$1\"'}' | grep id | cut -d '"' -f 4
 }
 
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+# Process phone photo.
+
+ppp() {
+
+    # Check if ImageMagick's convert command-line tool is installed.
+
+    if ! command -v "convert" $> /dev/null; then
+        printf "Please install ImageMagick's 'convert' command-line tool!"
+        exit;
+    fi
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    declare option="$1"
+    declare photo="${2:-*.jpg}"
+    declare geometry="${3:-50%}"
+
+    if [ "$option" != "clean" ] &&
+       [ "$option" != "resize" ]; then
+        option="resize"
+        photo="${1:-*.jpg}"
+        geometry="${2:-50%}"
+    fi
+
+    if [[ "$(echo "${photo##*.}" | tr '[:upper:]' '[:lower:]')" != "png" ]]; then
+        newPhotoName="${photo%.*}.png"
+    else
+        newPhotoName="_${photo%.*}.png"
+    fi
+
+    if [ "$option" == "resize" ]; then
+        convert "$photo" \
+            -colorspace RGB \
+            +sigmoidal-contrast 11.6933 \
+            -define filter:filter=Sinc \
+            -define filter:window=Jinc \
+            -define filter:lobes=3 \
+            -sigmoidal-contrast 11.6933 \
+            -colorspace sRGB \
+            -background transparent \
+            -gravity center \
+            -resize "$geometry" \
+            +append \
+            "$newPhotoName" \
+        && printf "* %s (%s)\n" \
+            "$newPhotoName" \
+            "$geometry"
+
+        return
+    fi
+
+    convert "$photo" \
+        -morphology Convolve DoG:10,10,0 \
+        -negate \
+        -normalize \
+        -blur 0x1 \
+        -channel RBG \
+        -level 10%,91%,0.1 \
+        "$newPhotoName" \
+        && printf "* %s\n" "$newPhotoName"
+
+}
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+# Search history.
+
+qh() {
+    #           ┌─ enable colors for pipe
+    #           │  ("--color=auto" enables colors only if
+    #           │  the output is in the terminal)
+    grep --color=always "$*" "$HISTFILE" |       less -RX
+    # display ANSI color escape sequences in raw form ─┘│
+    #       don't clear the screen after quitting less ─┘
+}
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+# Search for text within the current directory.
+
+qt() {
+    grep -ir --color=always "$*" --exclude-dir=".git" --exclude-dir="node_modules" . | less -RX
+    #     │└─ search all files under each directory, recursively
+    #     └─ ignore case
+}
+
+# qt
+# qh
+# ppp
 # video2gif
 # _man
 # gifify
