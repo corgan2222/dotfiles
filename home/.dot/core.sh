4a1c5378b95b3ac815f858b5cd08a50646a2b17d
Stefan Knaak <stefan@knaak.org>
2019-08-09 01:17:16 +0200

    raspi

--- home/.dot/core.sh
+++ home/.dot/core.sh
@@ -65,12 +65,12 @@ shootProfile(){
 				DIST=$(cat /etc/os-release | grep '^NAME' | awk -F=  '{ print $2 }' | sed -e "s/^\"//" -e "s/\"$//")
 				PSUEDONAME=$(cat /etc/os-release| grep '^VERSION_CODENAME' | awk -F=  '{ print $2 }' | sed -e "s/^\"//" -e "s/\"$//")
 				REV=$(cat /etc/os-release| grep '^VERSION_ID' | awk -F=  '{ print $2 }' | sed -e "s/^\"//" -e "s/\"$//")
-				
-				MODELL=$(echo /sys/firmware/devicetree/base/model)
-				#MODELL_TYPE=$(cat /etc/os-release | grep '^upnpmodelname' | awk -F=  '{ print $2 }' | sed -e "s/^\"//" -e "s/\"$//")
+								
+				MODELL_TYPE=$(cat /etc/os-release | grep '^upnpmodelname' | awk -F=  '{ print $2 }' | sed -e "s/^\"//" -e "s/\"$//")
 				#MODELL_SYSTEM=$(cat /etc/os-release | grep '^ID' | awk -F=  '{ print $2 }' )	
+				
 				IFS= read -r -d '' model </proc/device-tree/model || [[ $model ]]
-				MODELL=$(tr -d '\0' </proc/device-tree/model)
+				MODELL_SYSTEM=$(tr -d '\0' </proc/device-tree/model)
 
 				#MODELL=$(cat /etc/os-release | grep '^PRETTY_NAME=' | awk -F=  '{ print $2 }' | sed -e "s/^\"//" -e "s/\"$//")
 			fi
