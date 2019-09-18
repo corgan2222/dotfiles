360680b025c3e7cc996b4a0f932b88d29ae643c7
Stefan Knaak <stefan@knaak.org>
2019-09-18 16:22:25 +0200

    functions

--- home/.dot/.bash_functions.sh
+++ home/.dot/.bash_functions.sh
@@ -1921,3 +1921,55 @@ function checkSSLfromDomain()
 # shorturl
 
 #source ~/.dot/eachdir
+
+# -*- mode: sh -*-
+# shellcheck disable=SC2039
+#
+# Miscellaneous shell-related functions.
+
+
+# Print boolean value of the exit code of the command passed as argument.
+bool() {
+    "$@" && echo "true" || echo "false"
+}
+
+# Split directories in a PATH-like variable.
+split_path_dirs() {
+    local paths="$1"
+    # shellcheck disable=SC2001
+    echo "$paths" | sed 's/:/\n/g'
+}
+
+# Return whether a string is present in a list of strings.
+contains_string() {
+    local match="$1"
+    local strings="$2"
+
+    local s
+    for s in $strings; do
+        if [ "$s" == "$match" ]; then
+            return 0
+        fi
+    done
+    return 1
+}
+
+# Return whether the shell is interactive.
+is_interactive() {
+    [[ $- == *i* ]]
+}
+
+# Source a system file.  If no filename is given, list the available names.
+ss() {
+    local sourcedir="$SYSTEM_DIR/source"
+    if [ $# -eq 0 ]; then
+        ls -1 "$sourcedir"
+        return
+    fi
+
+    local file
+    for file in "$@"; do
+        # shellcheck disable=SC1090
+        . "$sourcedir/$file"
+    done
+}
