c369d8039376b2451a79f0cb8eaba928054c979d
Stefan Knaak <stefan@knaak.org>
2020-01-08 07:51:35 +0100

    Add files via upload
    headless deploy

---
+++ deploy_headless.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# bootstrap script to install Homeshick and you preferred castles to a new
+# system.
+
+tmpfilename="/tmp/${0##*/}.XXXXX"
+
+if type mktemp >/dev/null; then
+  tmpfile=$(mktemp $tmpfilename)
+else
+  tmpfile=$(echo $tmpfilename | sed "s/XX*/$RANDOM/")
+fi
+
+trap 'rm -f "$tmpfile"' EXIT
+
+castles+=("corgan2222/dotfiles")
+
+
+if [[ ! -f $HOME/.homesick/repos/homeshick/homeshick.sh ]]; then
+  if command -v git >/dev/null; then
+      git clone git://github.com/andsens/homeshick.git $HOME/.homesick/repos/homeshick
+  else
+    echo "no git found... path?"    
+  fi    
+fi
+
+source $HOME/.homesick/repos/homeshick/homeshick.sh
+
+for castle in "${castles[@]}"; do
+  homeshick clone "$castle"
+done
