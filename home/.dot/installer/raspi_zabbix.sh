56c1d7333b158ee7a4325f5c11a0a5aa95e17022
corgan2222 <stefan@knaak.org>
2020-01-14 10:59:42 +0100

    zabbix-installer

--- home/.dot/installer/raspi_zabbix.sh
+++ home/.dot/installer/raspi_zabbix.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
     #zabbix
-    sudo ap zabbix-agent
+    sudo apt-get install zabbix-agent
     sudo joe /etc/zabbix/zabbix_agentd.conf
     sudo adduser zabbix sudo
     sudo usermod -a -G sudo zabbix
     sudo usermod -a -G video zabbix
     sudo usermod -a -G docker zabbix
     sudo visudo
+    sudo touch /etc/sudoers.d/010_pi-nopasswd
+    sudo echo "pi ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers.d/010_pi-nopasswd
+    sudo echo "zabbix ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers.d/010_pi-nopasswd
     joe /etc/sudoers.d/010_pi-nopasswd
