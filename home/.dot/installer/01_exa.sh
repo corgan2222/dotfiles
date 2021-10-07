0e3379e69f4c1e93f73de14ac0ec8cc7bae9dd64
Stefan Knaak <stefan@knaak.org>
2021-10-07 19:34:20 +0200

    exa

--- home/.dot/installer/01_exa.sh
+++ home/.dot/installer/01_exa.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-wget https://github.com/ogham/exa/releases/download/v0.9.0/exa-linux-x86_64-0.9.0.zip
-unzip exa-linux-x86_64-0.9.0.zip
-sudo mv exa-linux-x86_64 /usr/local/bin/exa
-sudo rm exa-linux-x86_64-0.9.0.zip
+mkdir exa
+cd exa
+wget https://github.com/ogham/exa/releases/download/v0.10.0/exa-linux-x86_64-v0.10.0.zip 
+unzip exa-linux-x86_64-v0.10.0.zip
+sudo mv bin/exa /usr/local/bin/exa
+sudo rm exa-linux-x86_64-v0.10.0.zip
+sudo rm -R bin
+sudo rm -R man
+sudo rm -R completions
+exa
 
