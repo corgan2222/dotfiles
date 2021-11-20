aaa36d69de812ac33f13d1046504ae4848cc1cff
corgan2222 <stefan@knaak.org>
2021-11-20 12:21:38 +0100

    esec

--- home/.dot/installer/esec_onlogic.sh
+++ home/.dot/installer/esec_onlogic.sh
@@ -1,13 +1,30 @@
+#!/bin/bash
+sudo apt-get update
+sudo apt-get upgrade
+
+APPS="curl wget lnav joe jo htop git tree pv figlet unzip apt-transport-https ca-certificates curl software-properties-common mosquitto mosquitto-clients python3 python3-pip"
+
+for i in ${APPS}
+do
+    sudo apt-get install "$i" -y
+    if [[ $? -ne 0 ]]; then
+        printf "\033[0;31m ${i} failed \033[0m \n"        
+    else
+     printf "\032[0;31m ${i} failed \032[0m \n"	    
+    fi
+done
+
+
 wget https://download.teamviewer.com/download/linux/teamviewer_amd64.deb
 sudo apt install ./teamviewer_amd64.deb -y
-sudo apt install curl -y
-sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
+
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
 sudo apt update -y
 apt-cache policy docker-ce
 sudo apt install docker-ce -y
 sudo systemctl status docker
+
 sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 docker-compose --version
