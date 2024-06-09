eef9b00d85f5debe04e7c2b8127e78247120dd36
corgan2222 <stefan@knaak.org>
2024-06-09 07:45:32 +0200

    bat

--- home/.dot/installer/00_basics.sh
+++ home/.dot/installer/00_basics.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #sudo apt-get update
 #sudo apt-get upgrade
-APPS="curl wget lnav joe jo htop git tree pv figlet unzip mtr"
+APPS="curl wget lnav joe jo htop git tree pv figlet unzip mtr bat"
 
 for i in ${APPS}
 do
---
+++ home/.dot/installer/raspi-bat.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cd ~
+wget https://github.com/sharkdp/bat/releases/download/v0.24.0/bat_0.24.0_armhf.deb
+sudo dpkg -i bat_0.24.0_armhf.deb
