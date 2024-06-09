54a5011132758434f51c330abcf0917725213a4d
corgan2222 <stefan@knaak.org>
2024-06-09 07:16:04 +0200

    update

--- home/.dot/installer/01_exa.sh
+++ home/.dot/installer/01_exa.sh
@@ -2,10 +2,10 @@
 
 mkdir exa
 cd exa
-wget https://github.com/ogham/exa/releases/download/v0.10.0/exa-linux-x86_64-v0.10.0.zip 
-unzip exa-linux-x86_64-v0.10.0.zip
+wget https://github.com/ogham/exa/releases/download/v0.10.1/exa-linux-x86_64-v0.10.1.zip
+unzip exa-linux-x86_64-v0.10.1.zip
 sudo mv bin/exa /usr/local/bin/exa
-sudo rm exa-linux-x86_64-v0.10.0.zip
+sudo rm exa-linux-x86_64-v0.10.1.zip
 sudo rm -R bin
 sudo rm -R man
 sudo rm -R completions
