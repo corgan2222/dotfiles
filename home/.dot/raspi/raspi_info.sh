4c563ee0505ff086bdf63e5524cbca5623bc8846
stefan@knaak.org <stefan@knaak.org>
2024-06-25 00:19:32 +0200

    fix raspi_info

--- home/.dot/raspi/raspi_info.sh
+++ home/.dot/raspi/raspi_info.sh
@@ -8,7 +8,28 @@
 #sudo usermod -aG video zabbix
 # visudo zabbix  ALL=(ALL:ALL) ALL
 
+# Dynamically find vcgencmd
+VCGENCMD_PATH=$(which vcgencmd)
+if [ -z "$VCGENCMD_PATH" ]; then
+    echo "vcgencmd not found, searching further..."
+    VCGENCMD_PATH=$(find /opt/vc/bin /usr/bin /bin -name vcgencmd 2>/dev/null)
+    if [ -z "$VCGENCMD_PATH" ]; then
+        echo "vcgencmd still not found. Please ensure it is installed."
+        exit 1
+    fi
+fi
+
 case "$1" in
+        all)
+                echo "Gathering all system information..."
+                for item in fanSpeed boardversion boardrevision boardserialnumber cpuvoltage cpuclock cpumem firmwareversion gpumem sdcardtotalsize sdcardused sdcardusedpercent sdcardfree temperature lcd_info measure_clock_arm measure_clock_core measure_clock_h264 measure_clock_isp measure_clock_v3d measure_clock_uart measure_clock_emmc measure_clock_pixel measure_clock_vec measure_clock_hdmi measure_clock_dpi measure_volts_core measure_volts_sdram_p measure_volts_sdram_c measure_volts_sdram_i codec_enabled_H264 codec_enabled_MPG2 codec_enabled_WVC1 codec_enabled_MPG4 codec_enabled_MJPG codec_enabled_WMV9; do
+                OUTPUT="$($0 $item)"
+                if [ -z "$OUTPUT" ]; then
+                        OUTPUT="N/A"
+                fi
+                printf "%s: %s\n" "$item" "$OUTPUT"  # Notice the \n at the end for a new line
+                done
+                ;;
         fanSpeed)
                 # get the Hardware Version
                 cat /tmp/fanSpeed.value
@@ -27,40 +48,40 @@ case "$1" in
                 ;;
         cpuvoltage)
                 # Voltage in Volt
-                 /opt/vc/bin/vcgencmd measure_volts | tr -d "volt=" | tr -d "V"
+                 $VCGENCMD_PATH measure_volts | tr -d "volt=" | tr -d "V"
                 ;;
         cpuclock)
                 # CPU Clock Speed in Hz
-                # /opt/vc/bin/vcgencmd measure_clock arm | tr -d "frequency(48)="
-                 /opt/vc/bin/vcgencmd measure_clock arm | cut -d'='  -f 2-
+                # $VCGENCMD_PATH measure_clock arm | tr -d "frequency(48)="
+                 $VCGENCMD_PATH measure_clock arm | cut -d'='  -f 2-
                 ;;
         cpumem)
                 # CPU Memory in MByte
-                 vcgencmd get_mem arm | tr -d "arm=" | tr -d "M"
+                 $VCGENCMD_PATH get_mem arm | tr -d "arm=" | tr -d "M"
                 ;;
         firmwareversion)
                 # Just the naked String of the firmware Version
-                 vcgencmd version | grep version | cut -d " " -f 2
+                 $VCGENCMD_PATH version | grep version | cut -d " " -f 2
                 ;;
         gpumem)
                 # Graphics memeory in MByte
-                 vcgencmd get_mem gpu | tr -d "gpu=" | tr -d "M"
+                 $VCGENCMD_PATH get_mem gpu | tr -d "gpu=" | tr -d "M"
                 ;;
         sdcardtotalsize)
                 # Size of SD-Card in KByte
-                 df -P | grep /dev/root | tr -s " " " " | cut -d " " -f 2
+                df -P | grep " /$" | tr -s " " | cut -d " " -f 2
                 ;;
         sdcardused)
                 # Used Diskspace in KByte
-                 df -P | grep /dev/root | tr -s " " " " | cut -d " " -f 3
+                df -P | grep " /$" | tr -s " " | cut -d " " -f 3
                 ;;
         sdcardusedpercent)
                 # Used Diskspace in Percent
-                 df -P | grep /dev/root | tr -s " " " " | cut -d " " -f 5
+                df -P | grep " /$" | tr -s " " | cut -d " " -f 5
                 ;;
         sdcardfree)
-                # free Diskspace in KByte
-                 df -P | grep /dev/root | tr -s " " " " | cut -d " " -f 4
+                # Free Diskspace in KByte
+                df -P | grep " /$" | tr -s " " | cut -d " " -f 4
                 ;;
         temperature)
                 # Temperature in 1/1000 centigrade
@@ -68,58 +89,58 @@ case "$1" in
                 ;;
         lcd_info)
                 # Temperature in 1/1000 centigrade
-                 vcgencmd get_lcd_info
+                 $VCGENCMD_PATH get_lcd_info
                 ;;
         measure_clock_arm)
-                  vcgencmd measure_clock arm | cut -d'='  -f 2-
+                  $VCGENCMD_PATH measure_clock arm | cut -d'='  -f 2-
                ;;
         measure_clock_core)
-                  vcgencmd measure_clock core | cut -d'='  -f 2-
+                  $VCGENCMD_PATH measure_clock core | cut -d'='  -f 2-
                ;;
         measure_clock_h264)
-                  vcgencmd measure_clock h264 | cut -d'='  -f 2-
+                  $VCGENCMD_PATH measure_clock h264 | cut -d'='  -f 2-
                ;;
         measure_clock_isp)
-                  vcgencmd measure_clock isp | cut -d'='  -f 2-
+                  $VCGENCMD_PATH measure_clock isp | cut -d'='  -f 2-
                ;;
         measure_clock_v3d)
-                  vcgencmd measure_clock v3d | cut -d'='  -f 2-
+                  $VCGENCMD_PATH measure_clock v3d | cut -d'='  -f 2-
                ;;
         measure_clock_uart)
-                  vcgencmd measure_clock uart | cut -d'='  -f 2-
+                  $VCGENCMD_PATH measure_clock uart | cut -d'='  -f 2-
                ;;
         measure_clock_emmc)
-                  vcgencmd measure_clock emmc | cut -d'='  -f 2-
+                  $VCGENCMD_PATH measure_clock emmc | cut -d'='  -f 2-
                ;;
         measure_clock_pixel)
-                  vcgencmd measure_clock pixel | cut -d'='  -f 2-
+                  $VCGENCMD_PATH measure_clock pixel | cut -d'='  -f 2-
                ;;
         measure_clock_vec)
-                  vcgencmd measure_clock vec | cut -d'='  -f 2-
+                  $VCGENCMD_PATH measure_clock vec | cut -d'='  -f 2-
                ;;
         measure_clock_hdmi)
-                  vcgencmd measure_clock hdmi | cut -d'='  -f 2-
+                  $VCGENCMD_PATH measure_clock hdmi | cut -d'='  -f 2-
                ;;
         measure_clock_dpi)
-                  vcgencmd measure_clock dpi | cut -d'='  -f 2-
+                  $VCGENCMD_PATH measure_clock dpi | cut -d'='  -f 2-
                ;;
         measure_volts_core)
-                  vcgencmd measure_volts core | tr -d "volt=" | tr -d "V"
+                  $VCGENCMD_PATH measure_volts core | tr -d "volt=" | tr -d "V"
                ;;
         measure_volts_sdram_p)
-                  vcgencmd measure_volts sdram_p | tr -d "volt=" | tr -d "V"
+                  $VCGENCMD_PATH measure_volts sdram_p | tr -d "volt=" | tr -d "V"
                ;;
         measure_volts_sdram_c)
-                  vcgencmd measure_volts sdram_c | tr -d "volt=" | tr -d "V"
+                  $VCGENCMD_PATH measure_volts sdram_c | tr -d "volt=" | tr -d "V"
                ;;
         measure_volts_sdram_i)
-                  vcgencmd measure_volts sdram_i | tr -d "volt=" | tr -d "V"
+                  $VCGENCMD_PATH measure_volts sdram_i | tr -d "volt=" | tr -d "V"
                ;;
         measure_volts_sdram_i)
-                  vcgencmd measure_volts sdram_i | tr -d "volt=" | tr -d "V"
+                  $VCGENCMD_PATH measure_volts sdram_i | tr -d "volt=" | tr -d "V"
                ;;
         codec_enabled_H264)
-                DATA=$( vcgencmd codec_enabled H264 | cut -d'='  -f 2-)        
+                DATA=$( $VCGENCMD_PATH codec_enabled H264 | cut -d'='  -f 2-)        
                 if [ "$DATA" = "enabled" ]; then  
                         echo 1  
                 fi
@@ -129,7 +150,7 @@ case "$1" in
 
                ;;
         codec_enabled_MPG2)
-                 DATA=$( vcgencmd codec_enabled MPG2 | cut -d'='  -f 2-)
+                 DATA=$( $VCGENCMD_PATH codec_enabled MPG2 | cut -d'='  -f 2-)
                 if [ "$DATA" = "enabled" ]; then  
                         echo 1  
                 fi
@@ -138,7 +159,7 @@ case "$1" in
                 fi        
                ;;
         codec_enabled_WVC1)
-                 DATA=$( vcgencmd codec_enabled WVC1 | cut -d'='  -f 2-)
+                 DATA=$( $VCGENCMD_PATH codec_enabled WVC1 | cut -d'='  -f 2-)
                 if [ "$DATA" = "enabled" ]; then  
                         echo 1  
                 fi
@@ -147,7 +168,7 @@ case "$1" in
                 fi        
                ;;
         codec_enabled_MPG4)
-                 DATA=$( vcgencmd codec_enabled MPG4 | cut -d'='  -f 2-)
+                 DATA=$( $VCGENCMD_PATH codec_enabled MPG4 | cut -d'='  -f 2-)
                 if [ "$DATA" = "enabled" ]; then  
                         echo 1  
                 fi
@@ -156,7 +177,7 @@ case "$1" in
                 fi        
                ;;
         codec_enabled_MJPG)
-                 DATA=$( vcgencmd codec_enabled MJPG | cut -d'='  -f 2-)
+                 DATA=$( $VCGENCMD_PATH codec_enabled MJPG | cut -d'='  -f 2-)
                 if [ "$DATA" = "enabled" ]; then  
                         echo 1  
                 fi
@@ -165,7 +186,7 @@ case "$1" in
                 fi        
                ;;
         codec_enabled_WMV9)
-                 DATA=$( vcgencmd codec_enabled WMV9 | cut -d'='  -f 2-)
+                 DATA=$( $VCGENCMD_PATH codec_enabled WMV9 | cut -d'='  -f 2-)
                 if [ "$DATA" = "enabled" ]; then  
                         echo 1  
                 fi
@@ -181,11 +202,11 @@ case "$1" in
                 done
 
                 for src in arm core h264 isp v3d uart pwm emmc pixel vec hdmi dpi ; do
-                echo  "$src:\t$( vcgencmd measure_clock $src)" ;
+                echo  "$src:\t$( $VCGENCMD_PATH measure_clock $src)" ;
                 done
 
                 for id in core sdram_c sdram_i sdram_p ; do
-                echo  "$id:\t$( vcgencmd measure_volts $id)" ;
+                echo  "$id:\t$( $VCGENCMD_PATH measure_volts $id)" ;
                 done
 
                 for codec in H264 MPG2 WVC1 MPG4 MJPG WMV9 ; do
@@ -195,6 +216,7 @@ case "$1" in
                 ;;
         *)
                 echo "Usage: $N {boardversion|boardrevision|boardserialnumber|cpuvoltage|cpuclock|cpumem|firmwareversion|gpumem|sdcardtotalsize|sdcardused|sdcardusedpercent|sdcardfree|temperature|lcd_info}" >&2
+                echo "Usage: $N all" >&2
 esac
 
 
