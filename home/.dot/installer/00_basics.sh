a9e356ca5525a48cfa6fe768b6859bf985cdd010
stefan@knaak.org <stefan@knaak.org>
2024-06-25 02:49:33 +0200

    installer

--- home/.dot/installer/00_basics.sh
+++ home/.dot/installer/00_basics.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
-#sudo apt-get update
-#sudo apt-get upgrade
-APPS="curl wget lnav joe jo htop git tree pv figlet unzip mtr bat"
-
-for i in ${APPS}
-do
-    sudo apt-get install "$i" -y
-    if [[ $? -ne 0 ]]; then
-        printf "\033[0;31m ${i} failed \033[0m \n"        
-    fi
-done
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install curl wget lnav joe jo htop git tree pv figlet unzip mtr bat exa -y
 
