52ecaaa203052f91fd12f1c8acdf0d81dc4de6ba
stefan@knaak.org <stefan@knaak.org>
2024-06-25 02:39:05 +0200

    docker installer

--- home/.dot/installer/docker_raspi.sh
+++ home/.dot/installer/docker_raspi.sh
@@ -2,13 +2,21 @@
 
 docker_raspi.sh()
 {
+    # Add Docker's official GPG key:
     sudo apt-get update
-    sudo apt-get install apt-transport-https ca-certificates curl gnupg2 software-properties-common
+    sudo apt-get install ca-certificates curl
+    sudo install -m 0755 -d /etc/apt/keyrings
+    sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+    sudo chmod a+r /etc/apt/keyrings/docker.asc
 
-    curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
-    echo "deb [arch=armhf] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list
-        
+    # Add the repository to Apt sources:
+    echo \
+    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
+    $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+    sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
     sudo apt-get update
-    sudo apt-get install docker-ce
+    
+    sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+    sudo docker run hello-world
 }    
 docker_raspi.sh
--- home/.dot/installer/raspi-bat.sh
+++ home/.dot/installer/raspi-bat.sh
@@ -3,3 +3,4 @@
 cd ~
 wget https://github.com/sharkdp/bat/releases/download/v0.24.0/bat_0.24.0_armhf.deb
 sudo dpkg -i bat_0.24.0_armhf.deb
+rm bat_0.24.0_armhf.deb
